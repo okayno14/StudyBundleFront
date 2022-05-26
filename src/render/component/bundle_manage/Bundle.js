e5c8ef840f967deea4ba738810a7586ddabbfb4f
okayno14 <okayno14@gmail.com>
2022-05-26 18:11:35 +0700

    Сделал позиционирование для будущих элементов меню

--- src/render/component/bundle_manage/Bundle.js
+++ src/render/component/bundle_manage/Bundle.js
@@ -1,6 +1,22 @@
 import {React} from 'react'
+import './index.css'
 
 export const Bundle = (props)=>
 <div>
-	<h1>Редактор Бандлов</h1>
+	<h1 className='Header'>Редактор Бандлов</h1>
+	<div className='Container'>
+		<div>
+			<div >
+				<div className='Left'>BundleListSearch_Component</div>
+				<div className='Right'>BundleTextSearch_Component</div>
+			</div>
+		</div>
+		<div>
+			<div>
+				
+				<div className='Left'>BundleReport_Component</div>
+				<div className='Right'>BundleACL_Component</div>
+			</div>
+		</div>
+	</div>
 </div>
